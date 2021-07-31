53568866df3bcc02314cae4f7f291f493278072f
rfrancobruno <rfrancobruno@yahoo.com>
2021-07-30 23:26:47 -0300

    New commit tester

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
+
 //Teste de publicação GitHub
 //Teste commit GitHub 20210730 11:15 PM
+//Teste commit GitHub 20210730 11:24 PM
+
 /* 
   Trabalho AVD2 08/06/2021 -  Rodrigo Franco Bruno 20201417
   
